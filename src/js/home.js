a2c8c7494b242085a70ebb81f93a01d77f9290be
Jhon Guerrero <jhoguer@gmail.com>
2020-03-02 22:06:25 -0500

    Aprendiendo Promises y funciones asincronas

--- src/js/home.js
+++ src/js/home.js
@@ -7,110 +7,75 @@ function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
 
-const getUserAll = new Promise(function(todoBien, todoMal) {
-  // Llamar a un api
-  setTimeout(function() {
-    // Luego de 3 segundos
-    todoBien('Se acabo el tiempo')
+const getUserAll = new Promise((todoBien, todoMal) => {
+  setTimeout(() => {
+    todoBien('Se acabo el tiempo 5000')
   }, 5000)
 })
 
-const getUser = new Promise(function(todoBien, todoMal) {
-  // Llamar a un api
-  setTimeout(function() {
-    // Luego de 3 segundos
-    todoBien('Se acabo el tiempo 3')
+const getUser = new Promise((todoBien, todoMal) => {
+  setTimeout(() => {
+    todoBien('Se acabo el tiempo 3000')
   }, 3000)
 })
 
-/*
-getUser
-  .then(function() {
-    console.log('Todo esta bn en la vida')
-  })
-  .catch(function(messege) {
-    console.log(messege)
-  })   */
+// getUser
+//   .then(() => console.log('Todo esta bien en la vida'))
+//   .catch(msg => console.log(`Fallo!!! ${msg}`))
 
-  Promise.race([getUser, getUserAll,])
-    .then(function(message) {
-      console.log(message)
-    })
-    .catch(function(message) {
-      console.log(message)
-    })
+Promise.race([
+  getUser,
+  getUserAll
+])
+.then((message) => console.log(message))
+.catch((message) => console.log(message));
 
-    $.ajax('https://randomuser.me/api/', {
-      method: 'GET',
-      success: function(data) {
-        console.log(data)
-      },
-      error: function(error) {
-        console.log(error)
-      }
-    });
+// $.ajax('https://randomuser.me/api/sdsf', {
+//   method: 'GET',
+//   success: (data) => {
+//     console.log(data)
+//   },
+//   error: (error) => {
+//     console.log(error)
+//   }
+// })
 
-  
-
-fetch('https://randomuser.me/api/')
-.then(function(response) {
-  console.log(response)
-  return response.json()
-})
-.then(function(user) {
-  console.log('user', user.results[0].name.first)
-})
-.catch(function() {
-  console.log('algo falló')
-});
+// fetch('https://randomuser.me/api/')
+//   .then(response => {
+//     return response.json()
+//     // console.log(response)
+//   })
+//   .then(data => console.log(data.results[0].name.first))
+//   .catch(error => console.log(error))
 
 
-//Mezcla de async await y Promise
-/*
-(async function load() {
-  
+// const load = async () => {
+//   try {
+//     const response = await fetch('https://randomuser.me/api/');
+//     const data = await response.json();
+//     console.log(data.results[0].name.first)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
 
-  async function getData(url) {
-    const response = await fetch(url)
-    const data = await response.json()
+// load();
+
+const load = async () => {
+  const getData = async (gender) => {
+    const url = `https://yts.mx/api/v2/list_movies.json?genre=${gender}`;
+    const response = await fetch(url);
+    const data = await response.json();
     return data
+
   }
 
-   const actionList = await getData('https://yts.lt/api/v2/list_movies.json?genre=action')
-   console.log('actionList', actionList)
+  const actionList = await getData('action');
+  const horrorList = await getData('horror');
+  const animationList = await getData('animation');
+  console.log(actionList.data.movies);
+  console.log(horrorList);
+  console.log(animationList);
+}
 
-  getData('https://yts.lt/api/v2/list_movies.json?genre=adventure')
-    .then(function(data) {
-      console.log('terrorList', data)
-    })
-    .catch(function() {
-      console.log('Error en la peticion')
-    })
-  
-
- 
-})()
-*/
-(async function load() {
-  
-
-  async function getData(url) {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
-  }
-  
-  const actionList = await getData('https://yts.lt/api/v2/list_movies.json?genre=action')
-  const terrorList = await getData('https://yts.lt/api/v2/list_movies.json?genre=adventure')
-    const animationlist = await getData('https://yts.lt/api/v2/list_movies.json?genre=animation')
-    
-    // console.log('actionList', actionList)
-    // console.log('terrorList', terrorList)
-    // console.log('animationlist', animationlist)
-
-    console.log(actionList, terrorList, animationlist)
-
-  
-
- 
-})()
+load();
